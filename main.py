1e2f357a4575e3631732b65319e8ebf7f3a84188
pgebski <gebskipiotr@gmail.com>
2022-09-17 15:23:33 +0200

    initial

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, redirect, url_for, flash
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+
+@app.route("/")
+def home_page():
+    return render_template("index.html")
+
+
+def main():
+    app.run()
+
+if __name__ == "__main__":
+    main(debug=True)
