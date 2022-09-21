4118ecf6bf1a134c621bb959a2224dd9ebdae5cc
pgebski <gebskipiotr@gmail.com>
2022-09-21 20:18:17 +0200

    added resume button download

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash, send_from_directory
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
@@ -11,6 +11,11 @@ def home_page():
 
 def main():
     app.run()
+
+@app.route('/download')
+def download():    
+    return send_from_directory(directory='static', path='files/PiotrGebskiResume.pdf', as_attachment=True)
+
 
 if __name__ == "__main__":
     main()
